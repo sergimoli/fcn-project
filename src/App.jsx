299b98d2ea4d88e3fd29be7cbdba60af3a63699e
Sergi Molina <sergimoli@gmail.com>
2025-02-23 21:05:34 +0100

    commit everything

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from './components/Header'
+import SearchAthlete from './components/SearchAthlete'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <SearchAthlete />
+    </div>
+  )
+}
+
+export default App
